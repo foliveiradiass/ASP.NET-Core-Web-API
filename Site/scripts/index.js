d232975d64e260238b6efa001499bb0920884b97
Fabricio <foliveiradiass@gmail.com>
2018-05-23 23:33:25 -0300

    Projeto site

---
+++ Site/scripts/index.js
@@ -0,0 +1,68 @@
+const v = new Vue({
+    el: '#app',
+    data: {
+        title: 'Importação dos Dados de Aluno',
+        loading: false,
+        students: [],
+        getStudentEndPoint: 'http://localhost:62898/api/student',
+        importFileEndPoint: 'http://localhost:62898/api/student/import'
+    },
+    created() {
+        this.getStudent();
+    },
+    methods: {
+        //Get list student
+        getStudent: function () {
+
+            axios({ method: "GET", "url": this.getStudentEndPoint }).then(result => {
+                this.students = result.data;
+            }, error => {
+                console.error(error);
+            });
+        },
+
+        selectStudent: function (e) {
+            console.log($(e.target))
+        },
+
+        formatDate: function (value) {
+            return moment(String(value)).format('DD/MM/YYYY')
+        },
+
+        //Remove student
+        removeStudent: function (id, nome) {
+            if (confirm('Deseja remover o aluno "' + nome + '" ?')) {
+                axios({ method: "DELETE", "url": this.getStudentEndPoint + '/' + id }).then(result => {
+                    this.getStudent();
+                }, error => {
+                    alert('FALHA AO EXCLUIR')
+                });
+            }
+        },
+
+        //Import file
+        submitFile: function () {
+
+            let formData = new FormData();
+            formData.append('file', this.file);
+
+            axios.post(this.importFileEndPoint,
+                formData,
+                {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                }
+            ).then(function () {
+                alert('ARQUIVO IMPORTADO COM SUCESSO!');
+            })
+                .catch(function () {
+                    alert('FALHA NA IMPORTAÇÃO');
+                });
+        },
+
+        handleFileUpload() {
+            this.file = this.$refs.file.files[0];
+        }
+    }
+});
